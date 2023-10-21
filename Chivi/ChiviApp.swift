c922ea66c53b91bad697afd62a5183e0506ee113
Abraham Morales Arroyo <abrahamof606@gmail.com>
2023-10-21 11:05:19 -0500

    Initial Commit

---
+++ Chivi/ChiviApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiviApp.swift
+//  Chivi
+//
+//  Created by Abraham Morales Arroyo on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ChiviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chivi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chivi
+//
+//  Created by Abraham Morales Arroyo on 10/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
