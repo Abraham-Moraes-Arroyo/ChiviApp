474bb99c6c6af28ded12fee76c4229173adcce38
Abraham Morales Arroyo <abrahamof606@gmail.com>
2023-11-25 09:56:25 -0600

    Started working on the Fountain Blue line

---
+++ Chivi/individuallocations/crownfountain.swift
@@ -0,0 +1,89 @@
+//
+//  crownfountain.swift
+//  Chivi
+//
+//  Created by Abraham Morales Arroyo on 11/25/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct crownfountain: View {
+    var body: some View {
+        let tower = CLLocationCoordinate2D(
+            latitude: 41.881719,
+            longitude: -87.623746)
+        
+        
+        ScrollView{
+            
+            VStack{
+                
+                
+                
+                Map(){
+                    Marker ("Crown Fountain", systemImage: "pin", coordinate:
+                                tower)
+                    .tint (.red)
+                    
+                }
+                .frame(height: 350)
+                
+                Image("crown")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(.white, lineWidth: 4)
+                    }
+                    .shadow(radius: 9)
+                    .offset(y: -300)
+                    .padding(.bottom, -130)
+                    .frame(height:400)
+                
+                
+                VStack(alignment: .leading) {
+                    Text("Crown Fountain ")
+                        .font(.title)
+                    
+                    
+                    HStack {
+                        Text("Chicago, IL 60625")
+                        Spacer()
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    
+                    
+                    Divider()
+                    
+                    
+                    Text("About  ")
+                        .font(.title2)
+//https://www.chicago.gov/city/en/depts/dca/supp_info/chicago_s_publicartcrownfountaininmillenniumpark.html
+                    Text("The fountain consists of two 50-foot glass block towers at each end of a shallow reflecting pool. The towers project video images from a broad social spectrum of Chicago citizens, a reference to the traditional use of gargoyles in fountains, where faces of mythological beings were sculpted with open mouths to allow water, a symbol of life, to flow out..")
+                    Text("")
+                    Button("Go", action:{
+                        let latitude = 41.881719
+                        let longitude = -87.623746
+                        let url = URL(string: "maps://?saddr=&daddr=\(latitude),\(longitude)")
+                        if UIApplication.shared.canOpenURL(url!) {
+                            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                        }
+                    }).foregroundColor(.green)
+                        .font(.title)
+                    
+//                    41.97579° N, 87.69850° W
+                }
+                .offset(y:-340)
+                .padding()
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    crownfountain()
+}
---
+++ Chivi/menus/fontaineBlueLine.swift
@@ -0,0 +1,60 @@
+//
+//  fontaineBlueLine.swift
+//  Chivi
+//
+//  Created by Abraham Morales Arroyo on 11/25/23.
+//
+
+import SwiftUI
+
+struct fontaineBlueLine: View {
+    var body: some View {
+        VStack{
+            Text("In this route you will see")
+                .fontWeight(.heavy)
+            //            HStack{
+            //                if (coolRestaurant.isFavorite){
+            //                    Image(systemName: "star.fill")
+            //                        .foregroundStyle(.yellow)
+            //                }
+            List{
+                
+                HStack{
+                    Image("polonia")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    NavigationLink(destination: polonia()) {
+                        Text("Polonia Triangle")
+                        
+                    }
+                }
+                
+                HStack{
+                    Image("nicholasj")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    NavigationLink(destination: nicholasjfountain()) {
+                        Text("Nicholas J Melas Centennial Fountain")
+                        
+                    }
+                }
+                
+                HStack{
+                    Image("crown")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    NavigationLink(destination: crownfountain()) {
+                        Text("Crown Fountain")
+                        
+                    }
+                }
+                
+                
+            }// this is the end of list
+        }
+    }
+}
+
+#Preview {
+    fontaineBlueLine()
+}
