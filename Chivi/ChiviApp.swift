1092b413135e5055a7e9fdfcc33bb65c1daae838
rloza4 <rloza4@uic.edu>
2023-11-27 17:47:00 -0600

    FireBase Packages set up and Firebase connected to Chivi

--- Chivi/ChiviApp.swift
+++ Chivi/ChiviApp.swift
@@ -6,12 +6,59 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct ChiviApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+// Code to implement Firebase into Chivi
+// Commented out and stored here for later debugging purposes
+
+//import SwiftUI
+//import FirebaseCore
+//
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+//
+//@main
+//struct YourApp: App {
+//  // register app delegate for Firebase setup
+//  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//
+//
+//  var body: some Scene {
+//    WindowGroup {
+//      NavigationView {
+//        ContentView()
+//      }
+//    }
+//  }
+//}
