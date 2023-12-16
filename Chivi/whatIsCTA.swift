ec1cd5cb086d35cd435ea3370ade3f764efe0ccd
rloza4 <rloza4@uic.edu>
2023-12-16 00:38:02 -0600

    new file added whatIsCTA

---
+++ Chivi/whatIsCTA.swift
@@ -0,0 +1,18 @@
+//
+//  whatIsCTA.swift
+//  Chivi
+//
+//  Created by Rogelio Lozano on 12/15/23.
+//
+
+import SwiftUI
+
+struct whatIsCTA: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    whatIsCTA()
+}
