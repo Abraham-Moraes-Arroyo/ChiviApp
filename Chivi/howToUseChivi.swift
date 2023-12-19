37dba986e472a2c0af1e6e61367f9337fe7fba02
yacervantes <ycervantes2221@gmail.com>
2023-12-18 18:27:08 -0600

    Updated how to use Chivi file

--- Chivi/howToUseChivi.swift
+++ Chivi/howToUseChivi.swift
@@ -1,183 +1,143 @@
 //
-//  howToUseChivi.swift
-//  Chivi
+// howToUseChivi.swift
+// Chivi
 //
-//  Created by student on 12/15/23.
+// Created by student on 12/15/23.
 //
-
 import SwiftUI
-
 struct howToUseChivi: View {
-    var body: some View {
-        ScrollView{
-            VStack(alignment:.center){
-                Image("chivi")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 300, height: 200, alignment: .center)
-                Text("How To use Chivi")
-                    .minimumScaleFactor(0.1)
-                    .font(.system(size: 30, weight: .bold, design: .default))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                Image("howtoChivi1")
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 250, height:400, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 280, height: 350)
-                    )
-                
-                Text("Begin by going to the second tab of the screen where it is labeled: Tours.")
-                    .minimumScaleFactor(0.1)
-                    .font(.system(size: 15, design: .default))
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                //                    .frame(maxWidth:300)
-                //                    .frame(minWidth:100)
-                //                    .frame(width:300, height: 100, alignment: .center)
-                
-                Image("howto2")
-                    
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 280, height:260, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 310, height: 240, alignment: .center)
-                    )
-                
-                Text("Here is where all the tours are going to be located. Feel free to pick any, but for this example we have chosen this one.")
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(.white)
-                    .font(.system(size: 15, design: .default))
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                
-                Image("howto3")
-                    
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 280, height:260, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 310, height: 240, alignment: .center)
-                    )
-                
-                Text("Feel free to favorite any of the locations that you wish to visit later or that you enjoyed visiting. ")
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(.white)
-                    .font(.system(size: 15, design: .default))
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                
-                Image("howto4")
-                    
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 280, height:260, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 310, height: 240, alignment: .center)
-                    )
-                
-                Text("To see your favorites you need to tap on the switch labeled 'Favorites'. Nest lets see one of our favorite items and tap on it.")
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(.white)
-                    .font(.system(size: 15, design: .default))
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                
-                Image("howto5")
-                    
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 280, height:260, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 310, height: 240, alignment: .center)
-                    )
-                
-                Text("Here we have the address of the location, a map to get an understanding of what surrounds the location and a button to take us there. Lets tap on the button to take us to the location. ")
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(.white)
-                    .font(.system(size: 15, design: .default))
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                //                    .frame(maxWidth:300)
-                //                    .frame(minWidth:100)
-                //                    .frame(width:300, height: 100, alignment: .center)
-                
-                
-                
-                
-                Image("howto6")
-                    
-                    .renderingMode(.original)
-                    .resizable()
-                    .cornerRadius(25)
-                    .aspectRatio(contentMode:.fit)
-                    .frame(width: 280, height:260, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(Color(red: 0.8, green: 0.8, blue: 0.8))
-                            .frame(width: 310, height: 240, alignment: .center)
-                    )
-                
-                Text("Observe how there are multiple ways on arriving to our destination, for this example we happened to use the car, but ideally we can use multiple forms of transportation to get where we neeed.")
-                    .minimumScaleFactor(0.1)
-                    .foregroundColor(.white)
-                    .font(.system(size: 15, design: .default))
-                    .padding(10)
-                    .background(Color(red: 0.6, green: 0.4, blue: 1))
-                    .cornerRadius(15)
-                //                    .frame(maxWidth:300)
-                //                    .frame(minWidth:100)
-                //                    .frame(width:300, height: 100, alignment: .center)
-                
-                // button to press for youTube video on takig the buss
-                
-            
-
-                
-                
-             
-                
-            
-                    
-            }
-            .padding(30)
+  @AppStorage("currentPage") var currentPage = 1
+  var body: some View {
+    // For slide animation
+    ZStack{
+      //Changing Between Views
+      if currentPage == 1{
+        ExtractedView(image: "learnAndGo", title: "Learn & Go", detail: "Read up on the significance of the locations and press go to get directions", bgColor: Color(red: 0.863, green: 0.816, blue: 1))
+          .transition(.scale)
+      }
+      if currentPage == 2{
+        ExtractedView(image: "howto3", title: "Favorites", detail: "Star your favorite locations in Chicago", bgColor: Color(red: 0.863, green: 0.816, blue: 1))
+          .transition(.scale)
+      }
+      if currentPage == 3{
+        ExtractedView(image: "chooseATour1", title: "Choose A Tour", detail: "Chivi provides you with tours to explore Chicago", bgColor: Color(red: 0.863, green: 0.816, blue: 1))
+          .transition(.scale)
+      }
+      if currentPage == 4{
+        ExtractedView(image: "chooseALocation", title: "Choose A Location", detail: "Choose which locations to visit on your route from top to bottom", bgColor: Color(red: 0.863, green: 0.816, blue: 1))
+          .transition(.scale)
+      }
+    }
+    .overlay (
+      //Button
+      Button(action: {
+        // changing views
+        withAnimation(.easeInOut) {
+          // checking
+          if currentPage < totalPages{
+            currentPage += 1
+          }
+          else {
+            currentPage = 1
+          }
         }
-    }
-    }
-
-
-#Preview {
-    howToUseChivi()
+      }, label: {
+        Image(systemName: "chevron.right")
+          .font(.system(size: 20, weight: .semibold))
+          .foregroundColor(.black)
+          .frame(width: 60, height : 60)
+          .background(Color.white)
+          .clipShape(Circle())
+        // Circular Slider
+          .overlay (
+            ZStack{
+              Circle()
+                .stroke(Color.black.opacity(0.04), lineWidth: 4)
+              Circle()
+                .trim(from: 0, to: CGFloat(currentPage) / CGFloat(totalPages))
+                .stroke(Color.white, lineWidth: 4)
+                .rotationEffect(.init(degrees: -90))
+            }
+            .padding(-15)
+          )
+      })
+      .padding(.bottom, 50)
+      ,alignment: .bottom
+    )
+  }
 }
+#Preview {
+  howToUseChivi()
+}
+struct ExtractedView: View {
+  var image: String
+  var title: String
+  var detail: String
+  var bgColor: Color
+  @AppStorage("currentPage") var currentPage = 1
+  var body: some View {
+    VStack(spacing: 20){
+      HStack{
+        // Showing it only for first Page
+        if currentPage == 1{
+          Text("Welcome!")
+            .font(.title)
+            .fontWeight(.semibold)
+          //letter spacing
+            .kerning(1.4)
+        }
+        else{
+          // Back Button
+          Button(action: {
+            withAnimation(.easeInOut) {
+              currentPage -= 1
+            }
+          }, label: {
+            Image(systemName: "chevron.left")
+              .foregroundColor(.white)
+              .padding(.vertical, 10)
+              .padding(.horizontal)
+              .background(Color.black.opacity(0.4))
+              .cornerRadius(10)
+          })
+        }
+        Spacer()
+        Button(action: {
+          withAnimation(.easeInOut) {
+            currentPage = 4
+          }
+        }, label: {
+          Text("Skip")
+            .fontWeight(.semibold)
+            .kerning(1.2)
+        })
+      }
+      .foregroundColor(.black)
+      .padding()
+      Spacer(minLength: 0)
+      Text(title)
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.black)
+      Image(image)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        //.frame(width: 200, height: 200)
+        .overlay(
+          RoundedRectangle(cornerRadius: 5)
+            .stroke(Color.purple, lineWidth: 5)
+        )
+        .padding(.bottom)
+      Text(detail)
+        .fontWeight(.semibold)
+        .kerning(1.3)
+        .multilineTextAlignment(.center)
+      // Minimum Spacing When phone is reducing
+      Spacer(minLength: 120)
+    }
+    .background(bgColor.cornerRadius(10))
+  }
+  //Color(red: 0.863, green: 0.816, blue: 1)
+  //"Read up on the significance of the locations and press go to get directions"
+}
+var totalPages = 4
