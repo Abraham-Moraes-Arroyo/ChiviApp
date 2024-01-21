b4376175185fffc1a0975d9ab64d0a690a257190
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-01-21 08:03:31 -0600

    Changed the version so it can go to the appstore.

--- Chivi/number3.swift
+++ Chivi/number3.swift
@@ -103,7 +103,21 @@ struct number3: View {
                                         .foregroundColor(.white)
 
                     
-              
+                    //                    Popular Cites
+                                        HStack{
+                                            Image("rat")
+                                                .resizable()
+                                                .frame(width: 50, height: 50)
+                                            NavigationLink(destination: dynamictours(tourId: "OnlineRec")){
+                                                Text("Online Recomendations")
+                                                
+                                                
+                                            }
+                                        }// end of blueline
+                    //                    agora
+                    //                    41.868123, -87.623380
+                                        .listRowBackground(Color.blue)
+                                        .foregroundColor(.white)
                     
                 } // end of List
                 
